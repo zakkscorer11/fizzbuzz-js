91ee599d716ac30255ca266d6a83f2870225f3e7
Zakk <zscorer@jigsawcreate.com>
2021-02-17 14:31:25 +0000

    firstcommit

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,7 +3,7 @@
 // Here, we create our main function.
 function fizzbuzz() {
     
-    console.log('Welcome to FizzBuzz!');
+    console.log('Welcome to FizzBuzz, zakk!');
 
     // Put your code here...
 

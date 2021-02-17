fcb175a3c8576d67a995e9e6627742b6ca32aa3b
Zakk <zscorer@jigsawcreate.com>
2021-02-17 14:56:04 +0000

    fizzbuzznumberslist

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,10 +3,28 @@
 // Here, we create our main function.
 function fizzbuzz() {
     
-    console.log('Welcome to FizzBuzz, zakk!');
+    console.log('Welcome to FizzBuzz, Zakk!');
 
     // Put your code here...
-
+    for ( var i = 1; i <= 100; i++ )
+    {
+      if ( i%3 === 0 && i%5 === 0 )
+      {
+        console.log( i + " FizzBuzz" );
+      }
+      else if ( i%3 === 0 ) 
+      {
+        console.log(i+ " Fizz" );
+      }
+      else if ( i%5 === 0 ) 
+      {
+        console.log(i+ " Buzz" );
+      }
+      else
+      {
+        console.log(i);
+      }
+    }      
     
 }
 
